85b8598d006200e1cfb3205a3f36c81a92a794a3
prateeksahu147 <prateeksahu147@gmail.com>
2022-04-08 07:38:58 +0530

    update db to postgres

--- API/API/settings.py
+++ API/API/settings.py
@@ -79,11 +79,19 @@ WSGI_APPLICATION = 'API.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'NRDB',
+        'USER': 'postgres',
+        'PASSWORD' :'Prateek@97', 
+        'HOST' : 'localhost',
+        },
     }
-}
 
 
 # Password validation
